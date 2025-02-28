d2dcee0d4915ea1e2e16bb733266a9a57345ffbc
Jared Patterson <jpwesley34@gmail.com>
2025-02-28 12:58:42 -0800

    Explain solution.

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -24,9 +24,8 @@ module.exports = reverseString;
 // Please add your pseudocode to this file
 
 // Take in a String,
-
 //for each character in the string, place it in a new string as the first character.
-
 // return new string
 
 // And a written explanation of your solution
+//The eventual output is declared as an empty array. Then the input string is iterated through, being placed at the beginning of the output array. After all characters of the string have been place at the front of the array, in other words, place in the array in the reverse order, the array is returned as a joined string.
